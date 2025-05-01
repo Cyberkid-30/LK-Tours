9ba8d271586700cf3c6df2550ddb1e425f00040a
Jan Phandoh <janphandoh@gmail.com>
2025-05-01 13:49:53 +0000

    Add padding to the BusinessCard component for improved layout

--- src/components/business-card.jsx
+++ src/components/business-card.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const BusinessCard = () => {
   return (
-    <div className="flex-1 flex flex-col justify-center items-center">
+    <div className="flex-1 flex flex-col justify-center items-center px-10 md:px-0">
       <div className="flex flex-col gap-y-3 justify-center items-center p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 max-w-lg animate-grow">
         <img src="/person.svg" className="size-24 object-cover bg-white" />
         <h2 className="text-xl font-bold text-gray-800 mb-2">LK Tours</h2>
