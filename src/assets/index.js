53ded37064af805d540e2be44a1f92c71f399eae
Jan Phandoh <janphandoh@gmail.com>
2025-05-01 13:24:44 +0000

    Fix asset import paths for images and background in CSS

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
-import image1 from "../../public/image-1.jpg";
-import image2 from "../../public/image-2.jpg";
-import image3 from "../../public/image-3.jpg";
-import image4 from "../../public/image-4.jpg";
-import image5 from "../../public/image-5.jpg";
+import image1 from "/image-1.jpg";
+import image2 from "/image-2.jpg";
+import image3 from "/image-3.jpg";
+import image4 from "/image-4.jpg";
+import image5 from "/image-5.jpg";
 export { image1, image2, image3, image4, image5 };
